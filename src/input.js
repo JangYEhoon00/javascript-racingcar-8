903bb4896bbe393fab07b64c9b45fdd33da58370
JangYEhoon00 <rainer8191@gmail.com>
2025-10-26 23:34:27 +0900

    refactor(getName, getCount) : 이름과 시행회수에 따른 입력값 구분

--- src/input.js
+++ src/input.js
@@ -1,8 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
 
-export default async function getInput(message) {
-  const input_Value = await Console.readLineAsync(`${message} : `);
-  const trim_Input = input_Value.replaceAll(" ", "");
+export default async function getName() {
+  const GET_NAME = await Console.readLineAsync("이름을 입력하세요. : ");
+  const TRIM_Input = input_Value.replaceAll(" ", "");
 
-  return trim_Input;
+  if (!(trim_Input.length <= 5)) {
+    throw new Error("이름의 길이는 5자 이하입니다.");
+  }
+  return TRIM_Input;
+}
+
+export default function getCount(){
+   const GET_COUNT =  Console.readLineAsync("실행 횟수를 입력하세요. : ");
+   
+   return GET_COUNT
 }
