5ea319ccf9f7b53c47d39bbbad4d5d0e444e665d
JangYEhoon00 <rainer8191@gmail.com>
2025-10-27 23:40:30 +0900

    refactor(car.test): 전진시 위치 증가 테스트 추가

--- __tests__/car.test.js
+++ __tests__/car.test.js
@@ -1,59 +1,73 @@
 import CarInfo from "../src/carInfo.js";
 
 describe("CarInfo 클래스 테스트", () => {
-  test("자동차 이름이 5자 이하인 경우 정상적으로 생성된다", () => {
-    expect(() => new CarInfo("pobi")).not.toThrow();
-    expect(() => new CarInfo("12345")).not.toThrow();
+  describe("생성자 테스트", () => {
+    test("자동차 이름이 5자 이하인 경우 정상적으로 생성된다", () => {
+      expect(() => new CarInfo("pobi")).not.toThrow();
+      expect(() => new CarInfo("12345")).not.toThrow();
+    });
+
+    test("자동차 이름이 5자를 초과하면 예외가 발생한다", () => {
+      const car = new CarInfo("123456");
+      expect(async () => await car.carName("123456")).rejects.toThrow("[ERROR]");
+    });
+
+    test("자동차 이름이 빈 문자열이면 예외가 발생한다", () => {
+      const car = new CarInfo("");
+      expect(async () => await car.carName("")).rejects.toThrow("[ERROR]");
+      expect(async () => await car.carName("   ")).rejects.toThrow("[ERROR]");
+    });
   });
 
-  test("자동차 이름이 5자를 초과하면 예외가 발생한다", () => {
-    const car = new CarInfo("123456");
-    expect(async () => await car.carName("123456")).rejects.toThrow("[ERROR]");
+  describe("move 메서드 테스트", () => {
+    test("랜덤 값이 4 이상이면 전진한다", () => {
+      const CAR = new CarInfo("pobi");
+      CAR.move(4);
+      expect(CAR.getPosition()).toBe(1);
+
+      CAR.move(5);
+      expect(CAR.getPosition()).toBe(2);
+
+      CAR.move(3);
+      expect(CAR.getPosition()).toBe(2);
+    });
+
+    test("랜덤 값이 4 미만이면 멈춘다", () => {
+      const CAR = new CarInfo("pobi");
+      CAR.move(3);
+      expect(CAR.getPosition()).toBe(0);
+
+      CAR.move(0);
+      expect(CAR.getPosition()).toBe(0);
+    });
+
+    test("계속 전진하면 위치가 증가한다", () => {
+      const CAR = new CarInfo("test");
+      CAR.move(4);
+      CAR.move(5);
+      CAR.move(9);
+      expect(CAR.getPosition()).toBe(3);
+    });
   });
 
-  test("자동차 이름이 빈 문자열이면 예외가 발생한다", () => {
-    const car = new CarInfo("");
-    expect(async () => await car.carName("")).rejects.toThrow("[ERROR]");
-    expect(async () => await car.carName("   ")).rejects.toThrow("[ERROR]");
-  });
+  describe("getter 메서드 테스트", () => {
+    test("자동차 이름을 반환한다", () => {
+      const CAR = new CarInfo("pobi");
+      expect(CAR.getName()).toBe("pobi");
+    });
 
-  test("랜덤 값이 4 이상이면 전진한다", () => {
-    const CAR = new CarInfo("pobi");
-    CAR.move(4);
-    expect(CAR.getPosition()).toBe(1);
+    test("자동차 위치를 시각적으로 표현한다", () => {
+      const CAR = new CarInfo("pobi");
+      expect(CAR.getDisplayedPosition()).toBe("");
 
-    CAR.move(5);
-    expect(CAR.getPosition()).toBe(2);
+      CAR.move(4);
+      expect(CAR.getDisplayedPosition()).toBe("-");
 
-    CAR.move(3);
-    expect(CAR.getPosition()).toBe(2);
-  });
-
-  test("랜덤 값이 4 미만이면 멈춘다", () => {
-    const CAR = new CarInfo("pobi");
-    CAR.move(3);
-    expect(CAR.getPosition()).toBe(0);
-
-    CAR.move(0);
-    expect(CAR.getPosition()).toBe(0);
-  });
-
-  test("자동차 이름을 반환한다", () => {
-    const CAR = new CarInfo("pobi");
-    expect(CAR.getName()).toBe("pobi");
-  });
-
-  test("자동차 위치를 시각적으로 표현한다", () => {
-    const CAR = new CarInfo("pobi");
-    expect(CAR.getDisplayedPosition()).toBe("");
-
-    CAR.move(4);
-    expect(CAR.getDisplayedPosition()).toBe("-");
-
-    CAR.move(5);
-    expect(CAR.getDisplayedPosition()).toBe("--");
-    
-    CAR.move(3);
-    expect(CAR.getDisplayedPosition()).toBe("--");
+      CAR.move(5);
+      expect(CAR.getDisplayedPosition()).toBe("--");
+      
+      CAR.move(3);
+      expect(CAR.getDisplayedPosition()).toBe("--");
+    });
   });
 });
